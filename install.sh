ffcd74e95f2d5516c5d6f9d0a3c4b0fd860ab97b
entnuo <neptunemoon051@gmail.com>
2022-03-26 13:46:04 -0300

    install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# automatizing everything cuz im lazy
+
+cp .Xresources $HOME/
+cp .zshenv $HOME/
+
+cp .config/dunst $HOME/.config/
+cp .config/mpd $HOME/.config/
+cp .config/ncmpcpp $HOME/.config/
+cp .config/nvim $HOME/.config/
+cp .config/zsh $HOME/.config/
