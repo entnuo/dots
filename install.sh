5510f802bf9654ca65b1d06e0b39688f26f32f43
entnuo <neptunemoon051@protonmail.com>
2022-06-07 12:23:06 -0300

    install.sh: copying dracula icons to ~/.icons

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ cp -rv .config/rofi $HOME/.config/
 cp -rv .config/openbox/ $HOME/.config/
 cp -rv .config/tint2/ $HOME/.config/
 
+    cp -rv .icons/dracula/ $HOME/.icons/
+
 cp -rv .fonts/jetbrains-mono $HOME/.fonts/ && fc-cache -fv
 
 cp -rv .themes/ $HOME/.themes/
