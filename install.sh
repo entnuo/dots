e527110d154d1a47f0906b88ae53563ca46fc6fb
entnuo <neptunemoon051@protonmail.com>
2022-08-13 19:39:12 -0300

    added hard links to the files

--- install.sh
+++ install.sh
@@ -25,6 +25,32 @@ echo 'copying .themes to $HOME/.themes'
 cp -r .themes/ $HOME/.themes/
 echo -e 'done\n'
 
-echo -e 'copying .fonts to $HOME/.fonts and updating font cache\n'
+echo -e 'copying .fonts to $HOME/.fonts and updating font cache'
 cp -r .fonts/jetbrains-mono $HOME/.fonts/ && fc-cache -f
+echo -e 'done\n'
+
+# hard linking .config so it's easy to make changes and keep both dots and ~/.* updated
+echo 'hard linking files'
+# dunst
+ln $PWD/.config/dunst/dunstrc $HOME/.config/dunst/dunstrc
+# neovim
+ln $PWD/.config/nvim/init.vim $HOME/.config/nvim/init.vim
+# openbox
+ln $PWD/.config/openbox/autostart $HOME/.config/autostart
+ln $PWD/.config/openbox/menu.xml $HOME/.config/menu.xml
+ln $PWD/.config/openbox/rc.xml $HOME/.config/rc.xml
+# rofi
+ln $PWD/.config/rofi/config.rasi $HOME/.config/rofi/config.rasi
+# tint2
+ln $PWD/.config/tint2/dark.tint2rc $HOME/.config/tint2/dark.tint2rc
+ln $PWD/.config/tint2/repentance.tint2rc $HOME/.config/tint2/repentance.tint2rc
+# zsh
+ln $PWD/.config/zsh/.zlogin $HOME/.config/zsh/.zlogin
+ln $PWD/.config/zsh/.zshrc $HOME/.config/zsh/.zshrc
+ln $PWD/.config/zsh/aliases $HOME/.config/zsh/aliases
+# .Xresources
+ln $PWD/.Xresources $HOME/.Xresources
+# .zshenv
+ln $PWD/.zshenv $HOME/.zshenv
+
 echo 'all done, enjoy'
