f38f26b5fd9e5eb1f6725886545d022e4ad7f39f
entnuo <neptunemoon051@protonmail.com>
2023-04-08 03:56:43 -0300

    added bspwm and sxhkd packages

--- packages.sh
+++ packages.sh
@@ -13,9 +13,9 @@
 # intel-ucode will be installed, so remember to run `sudo grub-mkconfig -o /boot/grub/grub.cfg`
 # and tlp will be as well, so enable/start it (tlp.service)
 
-sudo pacman -S discord dmenu dunst fcitx5 fcitx5-qt fcitx5-gtk fcitx5-mozc feh ffmpeg firefox \
+sudo pacman -S bspwm discord dmenu dunst fcitx5 fcitx5-qt fcitx5-gtk fcitx5-mozc feh ffmpeg firefox \
     gufw intel-ucode keepassxc libsixel \
     mpv nnn numlockx network-manager-applet neovim noto-fonts noto-fonts-cjk noto-fonts-extra pandoc-cli pavucontrol picom polybar pulseaudio pulseaudio-alsa pulsemixer redshift \
-    scrot tlp thunar ttf-font-awesome ttf-joypixels ttf-liberation wine xfce4-power-manager xsettingsd yt-dlp zathura zathura-pdf-mupdf zsh
+    scrot sxhkd tlp thunar ttf-font-awesome ttf-joypixels ttf-liberation wine xfce4-power-manager xsettingsd yt-dlp zathura zathura-pdf-mupdf zsh
 
 yay -S ttf-symbola ttf-vlgothic hypnotix betterlockscreen nsxiv xfce4-volumed
